7069ac20852e0386c30e4396ca56e15f77764b17
Jony <liu.z.jonathan@gmail.com>
2017-10-06 13:51:40 -0700

    Now prints out important info from each headline
    it examines the page of each search results for the main article text
    (using unfluff).

--- headlines.js
+++ headlines.js
@@ -1,5 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const url = require('url');
+const extractor = require('unfluff');
 
 exports.getHeadlines = function(query) {
 	return new Promise( function(resolve, reject) {
@@ -9,6 +11,21 @@ exports.getHeadlines = function(query) {
 				var headlines = $('h3').map(function (i, elem) {
 					//this refers to elem
 					console.log('headline' + i + ': ', $(this).text());
+					var address = url.parse($(this).find('a').attr('href'), true).query.q;
+					request(address, function(error, response, body) {
+						var $ = cheerio.load(body);
+						var data = extractor(body);
+						console.log(address);
+						//if webpage doesn't include membership message:
+						if (data.text.indexOf('We hope you have enjoyed your complimentary access for the month') === -1) {
+							console.log(data.text);
+						}
+						console.log("----------------------------------------------------------------------------------------------------");
+						console.log("----------------------------------------------------------------------------------------------------");
+						console.log("----------------------------------------------------------------------------------------------------");
+					});
+					
+					
 					return $(this).text();
 				});
 				console.log('end');
